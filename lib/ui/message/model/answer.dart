6fc79821fc9a92820d2d0b817a8eb0937558f640
shun-shun123 <shunsuke15vb@gmail.com>
2020-09-06 01:10:48 +0900

    Modelクラスの作成

---
+++ lib/ui/message/model/answer.dart
@@ -0,0 +1,17 @@
+class Answer {
+  final String questionerId;
+  final String questionContent;
+  final String answer1;
+  final String answer2;
+  final int reviewScore;
+  final int selectedAnswerIndex;
+
+  Answer({
+    this.questionerId,
+    this.questionContent,
+    this.answer1,
+    this.answer2,
+    this.reviewScore,
+    this.selectedAnswerIndex,
+  });
+}
---
+++ lib/ui/message/model/question.dart
@@ -0,0 +1,17 @@
+class Question {
+  final String answererId;
+  final String questionContent;
+  final String answer1;
+  final String answer2;
+  final String godMessage;
+  final int selectedAnswerIndex;
+
+  Question({
+    this.answererId,
+    this.questionContent,
+    this.answer1,
+    this.answer2,
+    this.godMessage,
+    this.selectedAnswerIndex,
+  });
+}
