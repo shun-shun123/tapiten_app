baab0d5a26f1fecc994e115381a81cb8cc20db07
obaya884 <taku3.884@gmail.com>
2020-09-08 02:56:25 +0900

    仔羊を探してIDを書き込む処理とView変更

--- lib/ui/matching/matching_god.dart
+++ lib/ui/matching/matching_god.dart
@@ -1,4 +1,14 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+enum MatchingStatus {
+  searching,
+  success,
+  failure,
+}
 
 class MatchingGodPage extends StatefulWidget {
   @override
@@ -6,6 +16,81 @@ class MatchingGodPage extends StatefulWidget {
 }
 
 class _MatchingGodPageState extends State<MatchingGodPage> {
+  final auth = FirebaseAuth.instance;
+  final fireStore = FirebaseFirestore.instance;
+  User currentUser;
+  MatchingStatus status = MatchingStatus.searching;
+
+  void getCurrentUser() {
+    try {
+      final user = auth.currentUser;
+      if (user != null) {
+        currentUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void searchingSheep() async {
+    List waitingSheep = [];
+
+    await fireStore
+        .collection('matching')
+        .where('is_login', isEqualTo: true)
+        .where('is_waiting', isEqualTo: true)
+        .get()
+        .then((QuerySnapshot querySnapshot) => {
+              querySnapshot.docs.forEach((element) {
+                waitingSheep.add(element.id);
+              })
+            })
+        .catchError((error) => {
+              // TODO: エラー処理
+            });
+
+    print(waitingSheep);
+
+    if (waitingSheep.length != 0) {
+      final randomIndex = Random().nextInt(waitingSheep.length);
+      final targetSheepId = waitingSheep[randomIndex];
+      matchingSheep(targetSheepId);
+    } else {
+      status = MatchingStatus.failure;
+    }
+  }
+
+  void matchingSheep(String targetSheepId) {
+    fireStore
+        .collection('matching')
+        .doc(targetSheepId)
+        .update({'opponent_id': currentUser.uid})
+        .then((value) => {status = MatchingStatus.success})
+        .catchError((error) => {
+              // TODO: エラー処理
+            });
+  }
+
+  void successMatching() {
+    print("success matching!");
+    setState(() {
+      status = MatchingStatus.success;
+    });
+  }
+
+  Future getWaitingSheep() async {
+    List waitingSheep;
+
+    return waitingSheep;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+    searchingSheep();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +102,8 @@ class _MatchingGodPageState extends State<MatchingGodPage> {
             Image.asset(
               'images/sheep.png',
             ),
-            Text(
-              '仔羊を探しています',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black54,
-              ),
+            MatchingStatusMessage(
+              status: status,
             ),
             RaisedButton(
               child: Text('back'),
@@ -32,6 +113,35 @@ class _MatchingGodPageState extends State<MatchingGodPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MatchingStatusMessage extends StatelessWidget {
+  MatchingStatusMessage({this.status});
+
+  final MatchingStatus status;
+
+  String getStatusMessage(MatchingStatus status) {
+    var statusMessage = "";
+    if (status == MatchingStatus.searching) {
+      statusMessage = "仔羊を探しています";
+    } else if (status == MatchingStatus.success) {
+      statusMessage = "仔羊が見つかりました";
+    } else if (status == MatchingStatus.failure) {
+      statusMessage = "仔羊が見つかりませんでした";
+    }
+    return statusMessage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      getStatusMessage(status),
+      style: TextStyle(
+        fontSize: 18,
+        color: Colors.black54,
       ),
     );
   }
