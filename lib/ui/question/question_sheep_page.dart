78cd061310bd19e7010e832f05a87cbd36aa2241
obaya884 <taku3.884@gmail.com>
2020-09-08 21:00:54 +0900

    レイアウトをモックに近づけた

--- lib/ui/question/question_sheep_page.dart
+++ lib/ui/question/question_sheep_page.dart
@@ -11,25 +11,107 @@ class _QuestionSheepPageState extends State<QuestionSheepPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff909090),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'images/sheep.png',
-            width: 76,
-          ),
-          Text('迷いの内容'),
-          TextFormField(),
-          Text('選択肢1'),
-          TextFormField(),
-          Text('選択肢2'),
-          TextFormField(),
-          QuestionDecideButton(
-            isFillForm: true,
-            onPressed: () {},
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/sheep.png',
+              width: 120,
+            ),
+            SizedBox(height: 30),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '迷いの内容',
+                  style: kTitleTextStyle,
+                ),
+                TextFormField(
+                  style: kTextFormFieldTextStyle,
+                  decoration: kQuestionTextFormFieldDecoration,
+                  maxLines: 8,
+                  maxLength: 140,
+                ),
+                SizedBox(
+                  height: 28,
+                ),
+                Text(
+                  '選択肢1',
+                  style: kTitleTextStyle,
+                ),
+                TextFormField(
+                  style: kTextFormFieldTextStyle,
+                  decoration: kChoiceTextFormFieldDecoration.copyWith(
+                      hintText: '1つ目の選択肢を入力'),
+                ),
+                SizedBox(height: 18.0),
+                Text(
+                  '選択肢2',
+                  style: kTitleTextStyle,
+                ),
+                TextFormField(
+                  style: kTextFormFieldTextStyle,
+                  decoration: kChoiceTextFormFieldDecoration.copyWith(
+                      hintText: '2つ目の選択肢を入力'),
+                ),
+                SizedBox(height: 48),
+              ],
+            ),
+            QuestionDecideButton(
+              isFillForm: true,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+const kTitleTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 13,
+);
+
+const kTextFormFieldTextStyle = TextStyle(
+  color: Color(0xff707070),
+);
+
+const kChoiceTextFormFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  fillColor: Colors.white,
+  filled: true,
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff707070), width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff9FD53E), width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+);
+
+const kQuestionTextFormFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  fillColor: Colors.white,
+  filled: true,
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff707070), width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff9FD53E), width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+);
