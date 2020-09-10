c111d11bec463ad236423c78f950735b0fa1f07c
片山凌 <ryotrumpet328@gmail.com>
2020-09-11 04:31:56 +0900

    プロフ編集画面のUI修正

--- lib/ui/profile_edit/profile_edit_icon.dart
+++ lib/ui/profile_edit/profile_edit_icon.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tapiten_app/storage/user_mode.dart';
 
 class ProfileEditIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 60, bottom: 5),
-      child: Center(
-        child: Icon(
-          Icons.account_circle,
-          color: Colors.black,
-          size: 100,
+        padding: EdgeInsets.only(
+          top: 100,
+          bottom: (UserMode.isGod) ? 80 : 130,
         ),
-      ),
-    );
+        child: Container(
+          width: 90.0,
+          height: 90.0,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: (UserMode.isGod)
+                  ? AssetImage('images/god.png')
+                  : AssetImage('images/sheep.png'),
+            ),
+          ),
+        ));
   }
 }
