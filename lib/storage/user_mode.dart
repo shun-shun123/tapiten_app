f75d90a9245fad997448e5ff938b8cfa54b51fa9
shun-shun123 <shunsuke15vb@gmail.com>
2020-09-11 11:39:43 +0900

    UserModeのインスタンスをルートのウィジェットから作成して全ルートで辿れるようにした

--- lib/storage/user_mode.dart
+++ lib/storage/user_mode.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tapiten_app/storage/shared_preferences_keys.dart';
 
-class UserMode {
+class UserMode extends ChangeNotifier {
   // アプリ実行中常に神様モードか子羊モードかを判定するためのフラグとなる
   static bool isGod = true;
 
+  // インスタンス化されたUserModeからisGodにアクセスするためのゲッター
+  bool get isGodFlag => isGod;
+
   // SharedPreferencesへのセーブと共に、アプリ実行中にモード管理を行うstatic変数も更新する
   Future saveUserMode({@required bool isGod}) async {
+    print('-----saveUserMode is called-----');
     var sharedPreferences = await SharedPreferences.getInstance();
     sharedPreferences.setBool(SharedPreferencesKeys.userMode, isGod);
     UserMode.isGod = isGod;
+    notifyListeners();
   }
 
   // main()で一度呼び出される
@@ -18,7 +23,8 @@ class UserMode {
   // モードが変更された場合はstaticな変数をそのまま書き換えれば良い
   Future<bool> loadUserMode() async {
     var sharedPreferences = await SharedPreferences.getInstance();
-    UserMode.isGod = sharedPreferences.getBool(SharedPreferencesKeys.userMode) ?? true;
+    UserMode.isGod =
+        sharedPreferences.getBool(SharedPreferencesKeys.userMode) ?? true;
     return UserMode.isGod;
   }
 }
