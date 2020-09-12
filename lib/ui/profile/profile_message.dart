4b5cdc7bf1ee0559a6ccbcd75105395d383b28c1
obaya884 <taku3.884@gmail.com>
2020-09-12 14:08:29 +0900

    ProfileページコンポーネントのStateless化
    (cherry picked from commit 2fa51d0e5400f1483e097b72fc51e95bd482b362)

--- lib/ui/profile/profile_message.dart
+++ lib/ui/profile/profile_message.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tapiten_app/ui/profile_edit/profile_edit_view_model.dart';
 import 'package:tapiten_app/ui/question/styles/text_style.dart';
 
-class ProfileMessage extends StatefulWidget {
-  @override
-  _ProfileMessageState createState() => _ProfileMessageState();
-}
+class ProfileMessage extends StatelessWidget {
+  final String messageText;
 
-class _ProfileMessageState extends State<ProfileMessage> {
-  String messageText = '';
+  ProfileMessage({@required this.messageText});
 
   @override
   Widget build(BuildContext context) {
-    var viewModel = Provider.of<ProfileEditViewModel>(context);
-    messageText = (viewModel.message != null) ? viewModel.message : '';
-
     return Container(
       padding: EdgeInsets.only(bottom: 25),
       child: Center(
--- lib/ui/profile/profile_name.dart
+++ lib/ui/profile/profile_name.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:tapiten_app/storage/user_mode.dart';
-import 'package:tapiten_app/ui/profile_edit/profile_edit_view_model.dart';
 import 'package:tapiten_app/ui/question/styles/text_style.dart';
 
-class ProfileName extends StatefulWidget {
-  @override
-  _ProfileNameState createState() => _ProfileNameState();
-}
+class ProfileName extends StatelessWidget {
+  final String godName;
+  final String sheepName;
 
-class _ProfileNameState extends State<ProfileName> {
-  String godName = '';
-  String sheepName = '';
+  ProfileName({@required this.godName, @required this.sheepName});
 
-  @override
   Widget build(BuildContext context) {
-    var viewModel = Provider.of<ProfileEditViewModel>(context);
-    godName = (viewModel.godName != null) ? viewModel.godName : '';
-    sheepName = (viewModel.sheepName != null) ? viewModel.sheepName : '';
-
     return Container(
       child: Center(
         child: Text(
